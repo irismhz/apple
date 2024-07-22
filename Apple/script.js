bf291ab35589b611a2e1b1e0891992aa6238d6ec
irismhz <irismaharjan00@gmail.com>
2024-07-22 20:33:54 +0545

    Add files via upload

---
+++ Apple/script.js
@@ -0,0 +1,9 @@
+var password;
+var pass1="123";
+password=prompt('enter password','');
+if ( password == pass1)
+    alert ('correct password, click ok to enter');
+else
+ {
+    window.location="https//www.google.com";
+}
