a8bcc099366fccd3ba221b67abf1c37310a8043f
irismhz <irismaharjan00@gmail.com>
2024-07-22 22:36:01 +0545

    Delete Apple directory

--- Apple/script.js
+++
@@ -1,9 +0,0 @@
-var password;
-var pass1="123";
-password=prompt('enter password','');
-if ( password == pass1)
-    alert ('correct password, click ok to enter');
-else
- {
-    window.location="https//www.google.com";
-}
